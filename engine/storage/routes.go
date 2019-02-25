da0bcfebc00ee5a6139e2f5bf4ee4ce263e738f9
ktenzer <keith.tenzer@gmail.com>
2019-02-25 14:38:19 +0100

    updated docker files and some other fixes

--- engine/storage/routes.go
+++ engine/storage/routes.go
@@ -18,11 +18,16 @@ func NewRouter() *mux.Router {
  
     router := mux.NewRouter().StrictSlash(true)
     for _, route := range routes {
+        var handler http.Handler
+ 
+        handler = route.HandlerFunc
+        handler = Logger(handler, route.Name)
+ 
         router.
             Methods(route.Method).
             Path(route.Pattern).
             Name(route.Name).
-            Handler(route.HandlerFunc)
+            Handler(handler)
     }
  
     return router
