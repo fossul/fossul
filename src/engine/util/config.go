76c08c928b0ffe2489b413f2978733b6a3d09a6f
Keith Tenzer <keith.tenzer@gmail.com>
2019-03-04 01:20:56 +0100

    added more fields to config object

--- src/engine/util/config.go
+++ src/engine/util/config.go
@@ -2,10 +2,19 @@ package util
 
 type Config struct {
 	Profile string `json:"profile"`
+	BackupName string `json:"backupName"`
 	AppPlugin string `json:"appPlugin"`
 	StoragePlugin string `json:"storagePlugin"`
 	BackupRetentions []BackupRetention `json:"backupRetentions"`
-
+	PreAppQuiesceCmd string `json:"preAppQuiesceCmd,omitempty"`
+	AppQuiesceCmd string `json:"appQuiesceCmd,omitempty"`
+	PostAppQuiesceCmd string `json:"postAppQuiesceCmd,omitempty"`
+	BackupCreateCmd string `json:"backupCreateCmd,omitempty"`
+	PreAppUnquiesceCmd string `json:"preAppUnquiesceCmd,omitempty"`
+	AppUnquiesceCmd string `json:"appUnquiesceCmd,omitempty"`
+	PostAppUnquiesceCmd string `json:"postAppUnquiesceCmd,omitempty"`
+	SendTrapErrorCmd string `json:"sendTrapErrorCmd,omitempty"`
+	SendTrapSuccessCmd string `json:"sendTrapSuccessCmd,omitempty"`
 }
 
 type BackupRetention struct {
