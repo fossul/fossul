2f3064bfc2f7f99a5c9817d60ec748602eae21a8
ktenzer <keith.tenzer@gmail.com>
2019-06-11 17:22:37 +0200

    updated docker file with user homedir

--- scripts/setupFossulDemo.sh
+++ scripts/setupFossulDemo.sh
@@ -9,12 +9,15 @@ $CLI --profile postgres --action addProfile --credential-file $CREDENTIAL_FILE
 $CLI --profile postgres --config postgres --action addConfig --config-file $CONFIG_DIR/postgres/postgres/postgres.conf --credential-file $CREDENTIAL_FILE 
 $CLI --profile postgres --config postgres --action addPluginConfig --config-file $CONFIG_DIR/postgres/postgres/container-basic.so.conf --plugin container-basic.so --credential-file $CREDENTIAL_FILE 
 $CLI --profile postgres --config postgres --action addPluginConfig --config-file $CONFIG_DIR/postgres/postgres/postgres-dump.so.conf --plugin postgres-dump.so --credential-file $CREDENTIAL_FILE 
+$CLI --profile postgres --config postgres --action addPluginConfig --config-file $CONFIG_DIR/postgres/postgres/aws.so.conf --plugin aws.so --credential-file $CREDENTIAL_FILE 
 $CLI --profile mariadb --config mariadb --action addConfig --config-file $CONFIG_DIR/mariadb/mariadb/mariadb.conf --credential-file $CREDENTIAL_FILE 
 $CLI --profile mariadb --config mariadb --action addPluginConfig --config-file $CONFIG_DIR/mariadb/mariadb/container-basic.so.conf --plugin container-basic.so --credential-file $CREDENTIAL_FILE 
 $CLI --profile mariadb --config mariadb --action addPluginConfig --config-file $CONFIG_DIR/mariadb/mariadb/mariadb-dump.so.conf --plugin mariadb-dump.so --credential-file $CREDENTIAL_FILE 
+$CLI --profile postgres --config mariadb --action addPluginConfig --config-file $CONFIG_DIR/maraidb/mariadb/aws.so.conf --plugin aws.so --credential-file $CREDENTIAL_FILE 
 $CLI --profile mongo --config mongo --action addConfig --config-file $CONFIG_DIR/mongo/mongo/mongo.conf --credential-file $CREDENTIAL_FILE 
 $CLI --profile mongo --config mongo --action addPluginConfig --config-file $CONFIG_DIR/mongo/mongo/container-basic.so.conf --plugin container-basic.so --credential-file $CREDENTIAL_FILE 
 $CLI --profile mongo --config mongo --action addPluginConfig --config-file $CONFIG_DIR/mongo/mongo/mongo-dump.so.conf --plugin mongo-dump.so --credential-file $CREDENTIAL_FILE 
+$CLI --profile postgres --config mongo --action addPluginConfig --config-file $CONFIG_DIR/mongo/mongo/aws.so.conf --plugin aws.so --credential-file $CREDENTIAL_FILE 
 $CLI --profile mongo --config mongo --action addSchedule --cron-schedule "0,15,30,45 * * * *" --credential-file $CREDENTIAL_FILE --policy daily
 $CLI --profile mongo --config mongo --action addSchedule --cron-schedule "20 * * * *" --credential-file $CREDENTIAL_FILE --policy weekly
 $CLI --profile mariadb --config mariadb --action addSchedule --cron-schedule "0,15,30,45 * * * *" --credential-file $CREDENTIAL_FILE --policy daily
