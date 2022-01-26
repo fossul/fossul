dd1d1a01ffb24a382f67a012690172fff1590723
ktenzer <keith.tenzer@gmail.com>
2022-01-25 18:20:06 -0800

    implemented restore crd in clusterserviceoperator and fixed bug with snake_case no passing variables indentically from cr spec to operator playbook

--- scripts/fossul-docker-build.sh
+++ scripts/fossul-docker-build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Needs to be run from fossul root directory
 
 SERVER_IMAGE="fossul-server:latest"
 APP_IMAGE="fossul-app:latest"
 STORAGE_IMAGE="fossul-storage:latest"
 CLI_IMAGE="fossul-cli:latest"
+REPO="fossul"
 
 if [[ -z "${BUILD_ARGS}" ]]; then
 	export BUILD_ARGS="all"
@@ -13,73 +15,73 @@ echo " Building docker images"
 
 case $BUILD_ARGS in
   all)
-    sudo podman build -t quay.io/ktenzer/$SERVER_IMAGE -f src/engine/server .
+    sudo podman build -t quay.io/$REPO/$SERVER_IMAGE -f src/engine/server .
     if [ $? != 0 ]; then exit 1; fi
-    sudo podman build -t quay.io/ktenzer/$APP_IMAGE -f src/engine/app .
+    sudo podman build -t quay.io/$REPO/$APP_IMAGE -f src/engine/app .
     if [ $? != 0 ]; then exit 1; fi
-    sudo podman build -t quay.io/ktenzer/$STORAGE_IMAGE -f src/engine/storage .
+    sudo podman build -t quay.io/$REPO/$STORAGE_IMAGE -f src/engine/storage .
     if [ $? != 0 ]; then exit 1; fi
-    sudo podman build -t quay.io/ktenzer/$CLI_IMAGE -f src/cli .
+    sudo podman build -t quay.io/$REPO/$CLI_IMAGE -f src/cli .
     if [ $? != 0 ]; then exit 1; fi
 
     sudo podman login quay.io
     if [ $? != 0 ]; then exit 1; fi
 
-    sudo podman push quay.io/ktenzer/$SERVER_IMAGE
+    sudo podman push quay.io/$REPO/$SERVER_IMAGE
     if [ $? != 0 ]; then exit 1; fi
-    sudo podman push quay.io/ktenzer/$APP_IMAGE
+    sudo podman push quay.io/$REPO/$APP_IMAGE
     if [ $? != 0 ]; then exit 1; fi
-    sudo podman push quay.io/ktenzer/$STORAGE_IMAGE
+    sudo podman push quay.io/$REPO/$STORAGE_IMAGE
     if [ $? != 0 ]; then exit 1; fi
-    sudo podman push quay.io/ktenzer/$CLI_IMAGE
+    sudo podman push quay.io/$REPO/$CLI_IMAGE
     if [ $? != 0 ]; then exit 1; fi
 
     echo "Building docker images completed successfully"
     ;;
   server)
-    sudo podman build -t quay.io/ktenzer/$SERVER_IMAGE -f src/engine/server .
+    sudo podman build -t quay.io/$REPO/$SERVER_IMAGE -f src/engine/server .
     if [ $? != 0 ]; then exit 1; fi
 
     sudo podman login quay.io
     if [ $? != 0 ]; then exit 1; fi
 
-    sudo podman push quay.io/ktenzer/$SERVER_IMAGE
+    sudo podman push quay.io/$REPO/$SERVER_IMAGE
     if [ $? != 0 ]; then exit 1; fi
 
     echo "Building server docker image completed successfully"
     ;;
   app)
-    sudo podman build -t quay.io/ktenzer/$APP_IMAGE -f src/engine/app .
+    sudo podman build -t quay.io/$REPO/$APP_IMAGE -f src/engine/app .
     if [ $? != 0 ]; then exit 1; fi
 
     sudo podman login quay.io
     if [ $? != 0 ]; then exit 1; fi
 
-    sudo podman push quay.io/ktenzer/$APP_IMAGE
+    sudo podman push quay.io/$REPO/$APP_IMAGE
     if [ $? != 0 ]; then exit 1; fi
 
     echo "Building app docker image completed successfully"
     ;;
   storage)
-    sudo podman build -t quay.io/ktenzer/$STORAGE_IMAGE -f src/engine/storage .
+    sudo podman build -t quay.io/$REPO/$STORAGE_IMAGE -f src/engine/storage .
     if [ $? != 0 ]; then exit 1; fi
 
     sudo podman login quay.io
     if [ $? != 0 ]; then exit 1; fi
 
-    sudo podman push quay.io/ktenzer/$STORAGE_IMAGE
+    sudo podman push quay.io/$REPO/$STORAGE_IMAGE
     if [ $? != 0 ]; then exit 1; fi
 
     echo "Building storage docker image completed successfully"
     ;;
   cli)
-    sudo podman build -t quay.io/ktenzer/$CLI_IMAGE -f src/cli .
+    sudo podman build -t quay.io/$REPO/$CLI_IMAGE -f src/cli .
     if [ $? != 0 ]; then exit 1; fi
 
     sudo podman login quay.io
     if [ $? != 0 ]; then exit 1; fi
 
-    sudo podman push quay.io/ktenzer/$CLI_IMAGE
+    sudo podman push quay.io/$REPO/$CLI_IMAGE
     if [ $? != 0 ]; then exit 1; fi
 
     echo "Building cli docker image completed successfully"
